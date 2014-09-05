8e8bef2723476ebd04d0c9d9410b0eb7a2917675
gdare <gregory.dare27@gmail.com>
2014-09-05 10:11:56 -0700

    Lightning

--- Lightning.pde
+++ Lightning.pde
@@ -1,13 +1,49 @@
-void setup()
-{
-  size(300,300);
-}
-void draw()
-{
+int startPosX = 250;
+int startPosY = 150;
+int endPosX = startPosX;
+int endPosY = 0;
+float cloudPosX = 250;
+float cloudPosY = 150;
+int cloudSize = 50;
 
+void setup(){
+	size(500,500);
+    strokeWeight(1);
+    background(255);
+    noLoop();
 }
-void mousePressed()
-{
-
+void draw(){
+	
+    while(endPosY < 500){
+        strokeWeight(1);
+        stroke(0,150,100);
+        endPosX = startPosX + (int)random(-10,10);
+        endPosY = startPosY + (int)random(1,10);
+        line(startPosX,startPosY,endPosX,endPosY);
+        System.out.println(""+startPosX+", " +startPosY+ ", " +endPosX + ", " +endPosY);
+        startPosX = endPosX;
+        startPosY = endPosY;
+    }
+	cloud();
+}
+void mousePressed(){
+    background(255);
+    startPosX = (int)random(225,275);
+    startPosY = 150;
+    endPosX = startPosX;
+    endPosY = 0;
+    redraw();
+}
+void cloud(){
+    strokeWeight(2);
+    stroke(100,100,100);
+	fill(100,100,100);
+    ellipse(cloudPosX, cloudPosY,cloudSize,cloudSize);
+    ellipse(cloudPosX + 25, cloudPosY, cloudSize, cloudSize);
+    ellipse(cloudPosX - 25, cloudPosY, cloudSize, cloudSize);
+    ellipse(cloudPosX + 12.5, cloudPosY - 12.5, cloudSize, cloudSize);
+    ellipse(cloudPosX - 12.5, cloudPosY - 12.5, cloudSize, cloudSize);
+    ellipse(cloudPosX + 12.5, cloudPosY + 12.5, cloudSize, cloudSize);
+    ellipse(cloudPosX - 12.5, cloudPosY + 12.5, cloudSize, cloudSize);
 }
 
--- build-tmp/source/Lightning.java
+++ build-tmp/source/Lightning.java
@@ -14,19 +14,53 @@ import java.io.IOException;
 
 public class Lightning extends PApplet {
 
-public void setup()
-{
-  size(300,300);
-  noLoop();
+int startPosX = 250;
+int startPosY = 150;
+int endPosX = startPosX;
+int endPosY = 0;
+float cloudPosX = 250;
+float cloudPosY = 150;
+int cloudSize = 50;
+
+public void setup(){
+	size(500,500);
+    strokeWeight(1);
+    background(255);
+    noLoop();
 }
-public void draw()
-{
-  int x = (int)(Math.random() * 300);
-  ellipse(x,150,10,10);
+public void draw(){
+	
+    while(endPosY < 500){
+        strokeWeight(1);
+        stroke(0,150,100);
+        endPosX = startPosX + (int)random(-10,10);
+        endPosY = startPosY + (int)random(1,10);
+        line(startPosX,startPosY,endPosX,endPosY);
+        System.out.println(""+startPosX+", " +startPosY+ ", " +endPosX + ", " +endPosY);
+        startPosX = endPosX;
+        startPosY = endPosY;
+    }
+	cloud();
 }
-public void mousePressed()
-{
-  redraw();
+public void mousePressed(){
+    background(255);
+    startPosX = (int)random(225,275);
+    startPosY = 150;
+    endPosX = startPosX;
+    endPosY = 0;
+    redraw();
+}
+public void cloud(){
+    strokeWeight(2);
+    stroke(100,100,100);
+	fill(100,100,100);
+    ellipse(cloudPosX, cloudPosY,cloudSize,cloudSize);
+    ellipse(cloudPosX + 25, cloudPosY, cloudSize, cloudSize);
+    ellipse(cloudPosX - 25, cloudPosY, cloudSize, cloudSize);
+    ellipse(cloudPosX + 12.5f, cloudPosY - 12.5f, cloudSize, cloudSize);
+    ellipse(cloudPosX - 12.5f, cloudPosY - 12.5f, cloudSize, cloudSize);
+    ellipse(cloudPosX + 12.5f, cloudPosY + 12.5f, cloudSize, cloudSize);
+    ellipse(cloudPosX - 12.5f, cloudPosY + 12.5f, cloudSize, cloudSize);
 }
 
   static public void main(String[] passedArgs) {
